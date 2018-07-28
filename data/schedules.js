ab0445db774725532de4987fc710b7b3a3dfb1c2
bigocb@gmail.com <bigocb@gmail.com>
2018-07-28 18:30:53 -0400

    scheduler updates

--- data/schedules.js
+++ data/schedules.js
@@ -10,10 +10,35 @@ var options = {
 };
 
 //Weather Polling
-setInterval(function() {
+setInterval(function(req,res,next) {   
+  var pref_type = 'autopoll_weather';
+  var pref_value = 'yes';
+  db.one('select preff_value from prefs  where pref_type = $1 and preff_value = $2', [pref_type,pref_value]
+).then(function (data) {
+  var qval = JSON.stringify(data);
+  console.log(qval.length);
+    res.status(200)
+      .json({
+        status: 'success',
+        data: data,
+        message: 'Retrieved ONE puppy'
+      });
+  if(qval.length > 0 ) {
     request("http://localhost:3000/test", function(error, response, body) {
-    console.log(body);
+      console.log(body);
+    });
+    return "positive";
+
+  } else {
+    return "negative";
+  }   
+  })    .catch(function (err) {
+    return next(err);
   });
-  }, 600000);
+  
+  }, 150000);
   
 //
+
+
+
