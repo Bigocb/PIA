c94e9cffaf4df3065ce1c1a33fb8bf84746cf340
Robin Cloutier <bobby.cloutier@gmail.com>
2018-07-29 15:13:07 -0400

    update cleanup

--- database/base_tables.sql
+++
@@ -1,7 +0,0 @@
--- base tables
-
-create table fact_pia_weather (
-call_time varchar(255),
-source_key varchar(50),
-call_data varchar(1000000)
-)
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,17 +6,21 @@ var db = require('../../data/queries');
 var inb = require('../../data/inbound');
 var inb2 = require('../../data/controls/schedules');
 
-// theae will provide rhe content for tbe elements
 
+// outbound data routes 
 router.get('/api/puppies', db.getAllPuppies);
 router.get('/api/puppies/:id', db.getSinglePuppy);
 router.post('/api/puppies', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
 router.delete('/api/puppies/:id', db.removePuppy);
+
+// external API routes (inbound)
 router.get('/weather', inb.getWeather);
 router.get('/media', inb.getMedia);
 router.get('/news', inb.getNews);
 
+
+// main route
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(
     __dirname, '..', '..', 'client', 'views', 'index.html'));
