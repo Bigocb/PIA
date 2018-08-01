56f7c0927d9ab6d1f9f8600e76a6247f6fa32335
Robin Cloutier <bobby_cloutier@premierinc.com>
2018-08-01 11:51:46 -0400

    update to db config

--- database/connections.js
+++ database/connections.js
@@ -5,11 +5,18 @@ var options = {
   promiseLib: promise
 };
 
+var dbConfig = require('../database/config/config').get('main');
+var dbHostPassport = dbConfig.get('dbConfig.host');
+var dbPortPassport = dbConfig.get('dbConfig.port');
+var dbNamePassport = dbConfig.get('dbConfig.dbName');
+var dbUserPassport = dbConfig.get('dbConfig.user');
+var dbPasswordPassport = dbConfig.get('dbConfig.pass');
+
 monitor.attach(options); 
 
 // local postgres connection
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://postgres:Lscooter11@localhost:5432/pia';
+var connectionString = 'postgres://' + dbUserPassport + ':' + dbPasswordPassport + '@' + dbHostPassport +':' + dbPortPassport + '/' + dbNamePassport;
 var db = pgp(connectionString);
 
 module.exports = db;
