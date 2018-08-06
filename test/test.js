0f332ba7c809a150189f829f652f7bb9fcbd2727
bigocb@gmail.com <bigocb@gmail.com>
2018-08-05 21:49:28 -0400

    new test

--- test/test.js
+++ test/test.js
@@ -15,5 +15,14 @@ describe('Array', function() {
         done();
       });
   });
+  it('should list ALL blobs on /out/avgcondition GET', function(done) {
+    chai.request(server)
+      .get('/out/avgcondition ')
+      .end(function(err, res){
+        res.should.have.status(200);
+        done();
+      });
+  });
+
 });
 
