874f5ccf522d0071518ce040971fc1be9d155ad6
Robin Cloutier <bobby_cloutier@premierinc.com>
2018-08-06 16:35:36 -0400

    making variables const

--- server/data/analysis/logic.js
+++ server/data/analysis/logic.js
@@ -2,16 +2,19 @@ const express = require('express');
 var promise = require('bluebird');
 var db = require('../../database/connections');
 
+// weather vars
+const node = 'current_observation';
+const cat = 'weather'
+
+//general vars
+const dateFormat = 'yyyy-mm-dd';
+const day = 'date';
+
 function getAverageTemps(req, res, next) {
-  var day = 'date';
-  var cat = 'weather';
-  var node = 'current_observation';
   var tmp = 'temp_f';
-  var dateFormat = 'yyyy-mm-dd';
-  var interval = '4 hours';
   db.any('select ' +
       '((response_data -> $2)->>$3) as temp_f' +
-      ' from responses, prefs  where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date and pref_type = $4', [cat, node, tmp, day, dateFormat,interval])
+      ' from responses, prefs  where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date and pref_type = $4', [cat, node, tmp, day, dateFormat])
     .then(function (data) {
       var tar = [];
       for (var i = 0; i < data.length; i++) {
@@ -36,14 +39,9 @@ function getAverageTemps(req, res, next) {
 };
 
 function getAverageCondition(req, res, next) {
-  var day = 'date'
-  var cat = 'weather';
-  var node = 'current_observation';
-  var interval = '4 hours';
-  var dateFormat = 'yyyy-mm-dd';
   db.any('select ' +
       '((response_data -> $2)->>$1) as weather' +
-      ' from responses, prefs where category = $1 and (to_char(to_timestamp(response_key-14440),$4))::date = preff_value::date and pref_type = $3', [cat, node, day, dateFormat,interval])
+      ' from responses, prefs where category = $1 and (to_char(to_timestamp(response_key-14440),$4))::date = preff_value::date and pref_type = $3', [cat, node, day, dateFormat])
     .then(function (data) {
       var tar = [];
       for (var i = 0; i < data.length; i++) {
@@ -75,17 +73,11 @@ function getAverageCondition(req, res, next) {
 };
 
 function getAverageHumidity(req, res, next) {
-  var day = 'date';
-  var cat = 'weather';
-  var section = 'current_observation';
   var key = 'dewpoint_f';
-  var interval = '4 hours';
-  var dateFormat = 'yyyy-mm-dd';
-  var trimf = '%';
-  
+  var trimf = '%';  
   db.any('select ' +
       'trim($6 from ((response_data -> $2)->>$3))::int as humidity' +
-      ' from responses, prefs  where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date and pref_type = $4 ', [cat, section, key, day, dateFormat, trimf,interval])
+      ' from responses, prefs  where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date and pref_type = $4 ', [cat, section, key, day, dateFormat, trimf])
     .then(function (data) { //build array of dew_point readings
       var tar = [];
       for (var i = 0; i < data.length; i++) {
@@ -124,16 +116,13 @@ function getAverageHumidity(req, res, next) {
 
 
 function getTopNews(req, res, next) {
-  var day = 'date';
   var cat = 'news';
   var section = 'articles';
   var key = 'title';
-  var interval = '4 hours';
-  var dateFormat = 'yyyy-mm-dd';
   db.any('select ' +
       'b ->> $3 as title' +
       ' from responses a  join lateral jsonb_array_elements(response_data -> $2) b on true join prefs on (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date ' +
-      ' where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date  and pref_type = $4', [cat, section, key, day, dateFormat,interval])
+      ' where category = $1 and (to_char(to_timestamp(response_key-14440),$5))::date = preff_value::date  and pref_type = $4', [cat, section, key, day, dateFormat])
     .then(function (data) {
       var tar = [];
 
@@ -170,10 +159,8 @@ function getTopEvents(req, res, next) {
   var events = 'events';
   var name = 'name';
   var text = 'text';
-  var start = 'start'
-  var local = 'local'
-  var dateFormat = 'yyyy-mm-dd';
-  var pref_type = 'date';
+  var start = 'start';
+  var local = 'local';
   db.any('select distinct' +
       '(b-> $2) ->> $3 as event_name,' +
       'substring((b-> $4) ->> $5,0,11) as date' +
@@ -182,7 +169,7 @@ function getTopEvents(req, res, next) {
       'join prefs c on to_date(substring((b-> $4) ->> $5,0,11),$6) between c.preff_value::date and c.preff_value::date + 14 '+
       'and c.pref_type = $7 ' +
       'where category = $1 ' +
-      'order by 2 asc', [events, name, text, start, local, dateFormat,pref_type ])
+      'order by 2 asc', [events, name, text, start, local, dateFormat,day ])
     .then(function (data) {
       console.log(data);
       res.status(200)
@@ -196,10 +183,10 @@ function getTopEvents(req, res, next) {
 };
 
 function getDailyHealth(req, res, next) {
-  var date = 'date';
+
   db.any('select * from health_data a ' +
       'join prefs b on start = preff_value ' +
-      'where pref_type = $1', [date])
+      'where pref_type = $1', [day])
     .then(function (data) {
       console.log(data);
       res.status(200)
